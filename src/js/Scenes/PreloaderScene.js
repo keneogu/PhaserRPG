8d1069954ce41b0f9d1dc70786da2e2e73ef56d0
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-06-07 15:29:42 +0100

    modified preloadScene

--- src/js/Scenes/PreloaderScene.js
+++ src/js/Scenes/PreloaderScene.js
@@ -62,7 +62,7 @@ export default class PreloaderScene extends Phaser.Scene {
 
     // update progress bar
     this.load.on('progress', (value) => {
-      percentText.setText(`${parseInt(value * 100)}%`);
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
